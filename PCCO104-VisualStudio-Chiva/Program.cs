fda00e7245434ce820e408bb500494ceaae7aa55
USER <USER@DESKTOP-V1U2S5D>
2024-03-16 11:17:19 +0800

    Add project files.

---
+++ PCCO104-VisualStudio-Chiva/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PCCO104_VisualStudio_Chiva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string[] names = { "name1", "name2"};
+
+            foreach(var name in names)
+            {
+                Console.WriteLine($"{name}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
